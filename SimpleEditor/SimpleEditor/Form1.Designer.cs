f999c867ef8ae0f94e4296a467b9c8deae141dd8
ChristianPrivitera <Caramello54>
2018-11-19 14:21:38 +0100

    Modifica Frontend

--- SimpleEditor/SimpleEditor/Form1.Designer.cs
+++ SimpleEditor/SimpleEditor/Form1.Designer.cs
@@ -28,114 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Editor));
-			this.btnOpenFile = new System.Windows.Forms.Button();
-			this.txtPercorso = new System.Windows.Forms.TextBox();
-			this.rTxtBody = new System.Windows.Forms.RichTextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.btnSalva = new System.Windows.Forms.Button();
-			this.btnIndenta = new System.Windows.Forms.Button();
-			this.btnSalvaConNome = new System.Windows.Forms.Button();
-			this.btnChiudiFile = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// btnOpenFile
-			// 
-			this.btnOpenFile.Location = new System.Drawing.Point(12, 53);
-			this.btnOpenFile.Name = "btnOpenFile";
-			this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
-			this.btnOpenFile.TabIndex = 0;
-			this.btnOpenFile.Text = "Apri file";
-			this.btnOpenFile.UseVisualStyleBackColor = true;
-			this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
-			// 
-			// txtPercorso
-			// 
-			this.txtPercorso.Location = new System.Drawing.Point(70, 11);
-			this.txtPercorso.Name = "txtPercorso";
-			this.txtPercorso.ReadOnly = true;
-			this.txtPercorso.Size = new System.Drawing.Size(430, 20);
-			this.txtPercorso.TabIndex = 1;
-			// 
-			// rTxtBody
-			// 
-			this.rTxtBody.Location = new System.Drawing.Point(12, 93);
-			this.rTxtBody.Name = "rTxtBody";
-			this.rTxtBody.Size = new System.Drawing.Size(488, 223);
-			this.rTxtBody.TabIndex = 2;
-			this.rTxtBody.Text = "";
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 14);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(52, 13);
-			this.label1.TabIndex = 3;
-			this.label1.Text = "Percorso:";
-			// 
-			// btnSalva
-			// 
-			this.btnSalva.Location = new System.Drawing.Point(208, 54);
-			this.btnSalva.Name = "btnSalva";
-			this.btnSalva.Size = new System.Drawing.Size(75, 23);
-			this.btnSalva.TabIndex = 4;
-			this.btnSalva.Text = "Salva";
-			this.btnSalva.UseVisualStyleBackColor = true;
-			this.btnSalva.Click += new System.EventHandler(this.btnSalva_Click);
-			// 
-			// btnIndenta
-			// 
-			this.btnIndenta.Location = new System.Drawing.Point(425, 54);
-			this.btnIndenta.Name = "btnIndenta";
-			this.btnIndenta.Size = new System.Drawing.Size(75, 23);
-			this.btnIndenta.TabIndex = 5;
-			this.btnIndenta.Text = "Indenta";
-			this.btnIndenta.UseVisualStyleBackColor = true;
-			this.btnIndenta.Click += new System.EventHandler(this.btnIndenta_Click);
-			// 
-			// btnSalvaConNome
-			// 
-			this.btnSalvaConNome.Location = new System.Drawing.Point(305, 54);
-			this.btnSalvaConNome.Margin = new System.Windows.Forms.Padding(2);
-			this.btnSalvaConNome.Name = "btnSalvaConNome";
-			this.btnSalvaConNome.Size = new System.Drawing.Size(98, 23);
-			this.btnSalvaConNome.TabIndex = 6;
-			this.btnSalvaConNome.Text = "Salva con nome";
-			this.btnSalvaConNome.UseVisualStyleBackColor = true;
-			this.btnSalvaConNome.Click += new System.EventHandler(this.btnSalvaConNome_Click);
-			// 
-			// btnChiudiFile
-			// 
-			this.btnChiudiFile.Location = new System.Drawing.Point(106, 54);
-			this.btnChiudiFile.Margin = new System.Windows.Forms.Padding(2);
-			this.btnChiudiFile.Name = "btnChiudiFile";
-			this.btnChiudiFile.Size = new System.Drawing.Size(75, 24);
-			this.btnChiudiFile.TabIndex = 7;
-			this.btnChiudiFile.Text = "Chiudi file";
-			this.btnChiudiFile.UseVisualStyleBackColor = true;
-			this.btnChiudiFile.Click += new System.EventHandler(this.btnChiudiFile_Click);
-			// 
-			// Form_Editor
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(512, 329);
-			this.Controls.Add(this.btnChiudiFile);
-			this.Controls.Add(this.btnSalvaConNome);
-			this.Controls.Add(this.btnIndenta);
-			this.Controls.Add(this.btnSalva);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.rTxtBody);
-			this.Controls.Add(this.txtPercorso);
-			this.Controls.Add(this.btnOpenFile);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MinimumSize = new System.Drawing.Size(528, 368);
-			this.Name = "Form_Editor";
-			this.Text = "Simple Editor";
-			this.SizeChanged += new System.EventHandler(this.Editor_SizeChanged);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Editor));
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtPercorso = new System.Windows.Forms.TextBox();
+            this.rTxtBody = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalva = new System.Windows.Forms.Button();
+            this.btnIndenta = new System.Windows.Forms.Button();
+            this.btnSalvaConNome = new System.Windows.Forms.Button();
+            this.btnChiudiFile = new System.Windows.Forms.Button();
+            this.rBttJson = new System.Windows.Forms.RadioButton();
+            this.rBttXml = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(16, 65);
+            this.btnOpenFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(100, 28);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Apri file";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtPercorso
+            // 
+            this.txtPercorso.Location = new System.Drawing.Point(93, 14);
+            this.txtPercorso.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPercorso.Name = "txtPercorso";
+            this.txtPercorso.ReadOnly = true;
+            this.txtPercorso.Size = new System.Drawing.Size(427, 22);
+            this.txtPercorso.TabIndex = 1;
+            // 
+            // rTxtBody
+            // 
+            this.rTxtBody.AcceptsTab = true;
+            this.rTxtBody.Location = new System.Drawing.Point(16, 114);
+            this.rTxtBody.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rTxtBody.Name = "rTxtBody";
+            this.rTxtBody.Size = new System.Drawing.Size(649, 274);
+            this.rTxtBody.TabIndex = 2;
+            this.rTxtBody.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Percorso:";
+            // 
+            // btnSalva
+            // 
+            this.btnSalva.Location = new System.Drawing.Point(277, 66);
+            this.btnSalva.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSalva.Name = "btnSalva";
+            this.btnSalva.Size = new System.Drawing.Size(100, 28);
+            this.btnSalva.TabIndex = 4;
+            this.btnSalva.Text = "Salva";
+            this.btnSalva.UseVisualStyleBackColor = true;
+            this.btnSalva.Click += new System.EventHandler(this.btnSalva_Click);
+            // 
+            // btnIndenta
+            // 
+            this.btnIndenta.Location = new System.Drawing.Point(567, 66);
+            this.btnIndenta.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnIndenta.Name = "btnIndenta";
+            this.btnIndenta.Size = new System.Drawing.Size(100, 28);
+            this.btnIndenta.TabIndex = 5;
+            this.btnIndenta.Text = "Indenta";
+            this.btnIndenta.UseVisualStyleBackColor = true;
+            this.btnIndenta.Click += new System.EventHandler(this.btnIndenta_Click);
+            // 
+            // btnSalvaConNome
+            // 
+            this.btnSalvaConNome.Location = new System.Drawing.Point(407, 66);
+            this.btnSalvaConNome.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSalvaConNome.Name = "btnSalvaConNome";
+            this.btnSalvaConNome.Size = new System.Drawing.Size(131, 28);
+            this.btnSalvaConNome.TabIndex = 6;
+            this.btnSalvaConNome.Text = "Salva con nome";
+            this.btnSalvaConNome.UseVisualStyleBackColor = true;
+            this.btnSalvaConNome.Click += new System.EventHandler(this.btnSalvaConNome_Click);
+            // 
+            // btnChiudiFile
+            // 
+            this.btnChiudiFile.Location = new System.Drawing.Point(141, 66);
+            this.btnChiudiFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnChiudiFile.Name = "btnChiudiFile";
+            this.btnChiudiFile.Size = new System.Drawing.Size(100, 30);
+            this.btnChiudiFile.TabIndex = 7;
+            this.btnChiudiFile.Text = "Chiudi file";
+            this.btnChiudiFile.UseVisualStyleBackColor = true;
+            this.btnChiudiFile.Click += new System.EventHandler(this.btnChiudiFile_Click);
+            // 
+            // rBttJson
+            // 
+            this.rBttJson.AutoSize = true;
+            this.rBttJson.Location = new System.Drawing.Point(542, 15);
+            this.rBttJson.Name = "rBttJson";
+            this.rBttJson.Size = new System.Drawing.Size(66, 21);
+            this.rBttJson.TabIndex = 8;
+            this.rBttJson.Text = "JSON";
+            this.rBttJson.UseVisualStyleBackColor = true;
+            // 
+            // rBttXml
+            // 
+            this.rBttXml.AutoSize = true;
+            this.rBttXml.Location = new System.Drawing.Point(614, 15);
+            this.rBttXml.Name = "rBttXml";
+            this.rBttXml.Size = new System.Drawing.Size(57, 21);
+            this.rBttXml.TabIndex = 9;
+            this.rBttXml.Text = "XML";
+            this.rBttXml.UseVisualStyleBackColor = true;
+            // 
+            // Form_Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 405);
+            this.Controls.Add(this.rBttXml);
+            this.Controls.Add(this.rBttJson);
+            this.Controls.Add(this.btnChiudiFile);
+            this.Controls.Add(this.btnSalvaConNome);
+            this.Controls.Add(this.btnIndenta);
+            this.Controls.Add(this.btnSalva);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rTxtBody);
+            this.Controls.Add(this.txtPercorso);
+            this.Controls.Add(this.btnOpenFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(698, 442);
+            this.Name = "Form_Editor";
+            this.Text = "Simple Editor";
+            this.SizeChanged += new System.EventHandler(this.Editor_SizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -149,6 +181,8 @@
         private System.Windows.Forms.Button btnIndenta;
         private System.Windows.Forms.Button btnSalvaConNome;
         private System.Windows.Forms.Button btnChiudiFile;
+        private System.Windows.Forms.RadioButton rBttJson;
+        private System.Windows.Forms.RadioButton rBttXml;
     }
 }
 
